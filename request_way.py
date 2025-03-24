82290c7185a3e601173c82c6dc1a35f1358a7d55
Eli-Zxh <1796313642@qq.com>
2025-03-25 00:51:08 +0800

    添加requests写法

---
+++ request_way.py
@@ -0,0 +1,51 @@
+import requests
+import re
+
+urlcode_dict = {
+    '中国联通' : '%E8%81%94%E9%80%9A%E5%87%BA%E5%8F%A3',
+    '中国电信' : '%E7%94%B5%E4%BF%A1%E5%87%BA%E5%8F%A3',
+    '中国移动' : '%E7%A7%BB%E5%8A%A8%E5%87%BA%E5%8F%A3',
+    '校园网' : 'internet',
+}# 运营商字典
+
+def getLoginMsg():
+    url = 'http://192.168.2.135'
+    r = requests.get(url=url)
+    try:
+        pattern = "href='(\S+)'"
+        href = re.search(pattern=pattern, string=r.text).group(1)
+    except TypeError:
+        print('你已在线')
+    destination = href.split('?')[0]  # 跳转目标url
+    params = href.split('?')[1]  # params字符串
+    return params
+
+
+def login(userId, password, type):
+    url = 'http://192.168.2.135/eportal/InterFace.do?method=login'
+    paramstr = getLoginMsg()
+    params = {
+        "method": "login"
+    }
+    data = {
+        "userId": userId,
+        "password": password,
+        "service": urlcode_dict[type],# 服务名，如果是中国联通换成'联通出口'，电信换成'电信出口'，移动换成'移动出口'，使用urlcode编码
+        "queryString": paramstr,
+        "operatorPwd": "",
+        "operatorUserId": "",
+        "validcode": "",
+        "passwordEncrypt": "false"
+    }
+    r = requests.post(url=url, params=params, data=data)
+    if r.json()["result"] == "success":
+        print("login success")
+    else:
+        print("login fail")
+        print(r.json())
+
+if __name__ == "__main__":
+    userId= '2021151620070'#输入你的学号
+    password= '232693'#输入你的密码
+    type= '中国联通'#输入你的运营商选择['中国联通','中国移动','中国电信','校园网']
+    login(userId=userId,password=password,type=type)
